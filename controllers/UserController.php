e31492cadd57a65550198b66fef8a728706b82c8
Albora Izquierdo Hernández <148754699+Alboraizquiher@users.noreply.github.com>
2025-03-24 13:03:42 +0100

    metida la funcion de registro

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -25,6 +25,7 @@ class UserController
 {
     public $username;
     public $password;
+    public $name;
 
     public function __construct() {}
     public function login()
@@ -55,21 +56,78 @@ class UserController
 
     public function register()
     {
+        session_start();//inicio de sesión para poder guardar los datos del usuario.
+        
+        require 'db_connection.php';  // Se utilizan variables fijas para probar el registro antes de conectarlo con una base de datos.
+       
         // Que el método reciba los datos del usuario y los guarde en la base de datos.
-        $this->username = $_POST['username'];
-        $this->password = $_POST['password'];
 
-        if ($this->username != '' && $this->password != '') {
-            $_SESSION['username'] = $this->username;
-            $_SESSION['password'] = $this->password;
+        $name = trim($_POST['username']);
+        $fecha_born = trim($_POST['fecha_born']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
 
-            echo 'Register success';
-            return true;
-        } else {
-            echo 'Register failed';
-            header('Location: ../../register.html');
-            return false;
+
+        //Verificar que esten vacios
+
+        if (!empty($name)  && !empty($fecha_born) && !empty($email) && !empty($password)) {
+
+            //Verificar que el correo no esté registrado
+            
+            $stmt = $connection->prepare("SELECT * FROM users WHERE email = ?"); 
+            $stmt->bind_param('s', $email);
+            $stmt->execute();
+            $stmt->store_result();
+
+            if ($stmt->num_rows > 0) {
+                echo 'Error: Este correo ya está registrado.';
+                header('Location: ../../registro_error.html');
+                exit();
+            }
+            $stmt->close();
+
+
+            // Verificar que el nombre de usuario no esté registrado
+
+            $stmt = $connection->prepare("SELECT idusers FROM users WHERE name = ?");
+        $stmt->bind_param('s', $name);
+        $stmt->execute();
+        $stmt->store_result();
+        
+        // Si el nombre de usuario ya está registrado, se redirige a la página de registro con un mensaje de error.
+        if ($stmt->num_rows > 0) {
+            echo 'Error: El nombre de usuario ya está registrado.';
+            header('Location: ../../registro_nombre_existente.html');
+            exit();
         }
+        $stmt->close();
+    
+      //hashear la contraseña
+      $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT);
+
+        //Preparar la consulta 
+        $stmt = $connection->prepare("INSERT INTO users (name, password) VALUES (?, ?)"); // Se prepara la consulta con los datos del usuario.
+        $stmt->bind_param('ss', $this->name, $hashedPassword);
+
+         // Si la consulta se ejecuta correctamente, se redirige a la página de inicio de sesión.
+         
+        if ($stmt->execute()) {
+            $_SESSION['name'] = $name;
+            echo 'Register success';
+            header('Location: ../../html.html');
+            exit();
+
+        } else {
+            error_log('Error en el registro: ' . $stmt->error);
+            echo 'Error en el registro';
+        }
+        $stmt->close();
+        $connection->close();
+        } else {
+        echo 'Register failed';
+        header('Location: ../../loginnoregr.html');
+        exit();
+            }
     }
     public function logout()
     {
