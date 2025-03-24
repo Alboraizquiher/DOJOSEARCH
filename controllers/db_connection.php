51e79f5a9767cd72eac8910c4a2777d4b2344685
angelopozo <apozo2027@gmail.com>
2025-03-24 12:36:52 +0100

    fixed db_connection

--- controllers/db_connection.php
+++ controllers/db_connection.php
@@ -1,20 +1,15 @@
 <?php
 
-  $server = "10.118.2.73";
-  $user = "Admin";
-<<<<<<< HEAD
+$server = "10.118.2.73";
+$user = "Admin";
 $password = "admin < bm,bv.¬kjlm n5";
-=======
-$password = "admin";
->>>>>>> dd70019a02fc74086957fc34c2d04faf08e246d1
 $database = "auth_db";
 $port = "3306";
 
-$connection = new mysqli($server, $user, $password, $database,$port);
+$connection = new mysqli($server, $user, $password, $database, $port);
 
 if ($connection->connect_error) {
-    die("No funciona na " . $connection->connect_error);
-}else{
-    echo "Conectado exitosamente";
+  die("No funciona na " . $connection->connect_error);
+} else {
+  echo "Conectado exitosamente";
 }
-?>
