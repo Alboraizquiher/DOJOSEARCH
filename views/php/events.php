2f3c2c72a117a0e1360a7d49a5e9e86f8a4c18ff
Abril Palau Pardillos <abrilpalau14@gmail.com>
2025-05-30 16:57:55 +0200

    delete and redirect to the home page

--- views/php/events.php
+++ views/php/events.php
@@ -12,6 +12,20 @@ session_set_cookie_params([
 session_start();
 require_once '../../controllers/UserController.php';
 UserController::checkSession();
+
+if (isset($_GET['deleted']) && $_GET['deleted'] === '1') {
+    $_SESSION = [];
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();
+    header("Location: /DojoSearch/views/php/index.php");
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
